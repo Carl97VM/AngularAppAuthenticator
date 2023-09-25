f37a3011cf511e09ab1cc385dbaf976654d95967
Carl97VM <barcacarlosv@gmail.com>
2023-09-25 19:40:30 -0400

    guard de validacion de una ruta dashboard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -10,8 +11,9 @@ import Swal from 'sweetalert2'
 export class LoginPageComponent {
 
   // Formularios Reactivos
-  private fb = inject( FormBuilder );
+  private fb          = inject( FormBuilder );
   private authService = inject( AuthService );
+  private router      = inject( Router );
 
 
   public myForm: FormGroup = this.fb.group({
@@ -22,7 +24,7 @@ export class LoginPageComponent {
   login() {
     const { email, password } = this.myForm.value;
     this.authService.login(email, password).subscribe({
-      next:() => console.log('Todo Bien!'),
+      next:() => this.router.navigateByUrl('/dashboard'),
       error: (message) => {
         Swal.fire("Error", message,"error");
       }
